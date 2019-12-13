7f05753add7e825ddf78734ff3e8aa3faf7e7240
Jason Shuler <jshuler@gmail.com>
2019-12-13 09:48:56 -0500

    lets try timer 6

--- panda/board/main.c
+++ panda/board/main.c
@@ -649,13 +649,13 @@ uint64_t tcnt = 0;
 
 
 
-void TIM5_IRQHandler(void) {
-  if (TIM5->SR == 0) return;
-  puts("TIM5_IRQHandler: ");
+void TIM6_DAC_IRQHandler(void) {
+  if (TIM6->SR == 0) return;
+  puts("TIM6_DAC_IRQHandler: ");
   puth(TIM2->CNT);
   puts("\n");
   lkas_send();
-  TIM5->SR = 0;
+  TIM6->SR = 0;
 }
 
 
@@ -772,10 +772,6 @@ int main(void) {
   TIM2->PSC = 48-1;
   TIM2->CR1 = TIM_CR1_CEN;
   TIM2->EGR = TIM_EGR_UG;
-
-  TIM5->PSC = 480-1;
-  TIM5->CR1 = TIM_CR1_CEN;
-  TIM5->EGR = TIM_EGR_UG;
   // use TIM2->CNT to read
 
 
@@ -809,8 +805,8 @@ int main(void) {
   timer_init(TIM9, 1464);
   NVIC_EnableIRQ(TIM1_BRK_TIM9_IRQn);
   //Timer for LKAS pump
-  //timer_init(TIM5, 15);
-  NVIC_EnableIRQ(TIM5_IRQn);
+  timer_init(TIM6, 15);
+  NVIC_EnableIRQ(TIM6_DAC_IRQn);
 
 #ifdef DEBUG
   puts("DEBUG ENABLED\n");
