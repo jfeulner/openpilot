015f66009d4edf6a0a056d63458c368c411fb708
jshuler <jshuler@gmail.com>
2019-12-22 17:56:59 -0500

    mathys lol

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -419,7 +419,7 @@ static CAN_FIFOMailBox_TypeDef * gm_lkas_hook(void) {
   }
 
   //update the rolling counter
-  current_lkas.RDLR = (0xFFFFFFFU & current_lkas.RDLR) + (lkas_rolling_counter << 28);
+  current_lkas.RDLR = (0xFFFFFFU & current_lkas.RDLR) + (lkas_rolling_counter << 24);
 
 //Thanks Andrew C
 
