79b82aef640e7ded9d0c52064f93743491e732f5
jshuler <jshuler@gmail.com>
2019-12-22 17:51:42 -0500

    gah!

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -419,7 +419,7 @@ static CAN_FIFOMailBox_TypeDef * gm_lkas_hook(void) {
   }
 
   //update the rolling counter
-  current_lkas.RDLR = (0xFFFU & current_lkas.RDLR) + (lkas_rolling_counter << 12);
+  current_lkas.RDLR = (0xFFFFFFFU & current_lkas.RDLR) + (lkas_rolling_counter << 28);
 
 //Thanks Andrew C
 
