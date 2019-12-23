104605c670e6f3795f3dd165711bc545ee12ec93
jshuler <jshuler@gmail.com>
2019-12-23 12:27:15 -0500

    bits are stupid

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -419,7 +419,7 @@ static CAN_FIFOMailBox_TypeDef * gm_lkas_hook(void) {
   }
 
   //update the rolling counter
-  current_lkas.RDLR = (0xFFFFFF & current_lkas.RDLR) + (lkas_rolling_counter << 28);
+  current_lkas.RDLR = (0xFFFFFFCF & current_lkas.RDLR) | (lkas_rolling_counter << 4);
 
 //Thanks Andrew C
 
